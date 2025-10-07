0f2870cd88e0034f8508a4f6554500cd7657f2b1
jo <ljonas@riseup.net>
2025-10-07 20:28:46 +0200

    chore: only publish chart once

--- Makefile
+++ Makefile
@@ -4,4 +4,10 @@ package:
 ifndef VERSION
 	$(error "VERSION is not defined")
 endif
-	touch package-$(VERSION).tgz
+	echo $(VERSION) > package-$(VERSION)
+	tar -cvzf package-$(VERSION).tgz package-$(VERSION)
+	rm -f package-$(VERSION)
+	cp package-$(VERSION).tgz other-$(VERSION).tgz
+
+clean:
+	rm -Rf dist package-* other-*
--- publish-package.sh
+++ publish-package.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+CHART_FILE="$1"
+
+if [[ -z "$CHART_FILE" ]]; then
+    echo "Usage: $0 <name-of-chart.tgz>" >&2
+    exit 1
+fi
+
+# Ensures we only publish when $1 == $GORELEASER_ARTIFACT_FILE
+if [[ -n "${GORELEASER_ARTIFACT_FILE}" && "${GORELEASER_ARTIFACT_FILE}" != "${CHART_FILE}" ]]; then
+    echo "skipping artifact: ${GORELEASER_ARTIFACT_FILE}" >&2
+    exit 0
+fi
+
 {
     echo "Publishing!"
     echo "$@"
-    env
-} >/dev/stderr
+} >&2
+
+exit 1
